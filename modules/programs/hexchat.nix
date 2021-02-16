241cbb8a46e92a3a2acb0b8a9aac415f327c0af1
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-02-15 21:09:50 -0300

    hexchat: refactor module
    - Improve documentation
    - Declare submodules at module top-level
    - Declare helpers at module top-level
    - Improve formatting

--- modules/programs/hexchat.nix
+++ modules/programs/hexchat.nix
@@ -1,131 +1,208 @@
 { config, pkgs, lib, ... }:
+
 with builtins;
 with lib;
-let cfg = config.programs.hexchat;
+let
+  cfg = config.programs.hexchat;
+
+  # Submodules
+  channelOptions = with types;
+    submodule {
+      options = {
+        autoconnect = mkOption {
+          type = nullOr bool;
+          description = "Autoconnect to network";
+          default = false;
+        };
+
+        connectToSelectedServerOnly = mkOption {
+          type = nullOr bool;
+          description = "Connect to selected server only";
+          default = true;
+        };
+
+        bypassProxy = mkOption {
+          type = nullOr bool;
+          description = "Bypass proxy";
+          default = true;
+        };
+
+        forceSSL = mkOption {
+          type = nullOr bool;
+          description = "Use SSL for all servers";
+          default = false;
+        };
+
+        acceptInvalidSSLCertificates = mkOption {
+          type = nullOr bool;
+          description = "Accept invalid SSL certificates";
+          default = false;
+        };
+
+        useGlobalUserInformation = mkOption {
+          type = nullOr bool;
+          description = "Use global user information";
+          default = false;
+        };
+      };
+    };
+
+  modChannelOption = with types;
+    submodule {
+      options = {
+        autojoin = mkOption {
+          type = listOf str;
+          default = [ ];
+          description = "Channels list to autojoin on connecting to server.";
+          example = literalExample ''[ "#home-manager" "#linux" "#nix" ]'';
+        };
+
+        charset = mkOption {
+          type = nullOr str;
+          default = null;
+          description = "Charset";
+          example = "UTF-8 (Unicode)";
+        };
+
+        commands = mkOption {
+          type = listOf str;
+          description = "Commands to be executed on connecting to server.";
+          example = literalExample ''[ "ECHO Greetings fellow Nixer! ]'';
+          default = [ ];
+        };
+
+        loginMethod = mkOption {
+          type = nullOr (enum [
+            "nickServMsg"
+            "nickServ"
+            "challengeAuth"
+            "sasl"
+            "serverPassword"
+            "saslExternal"
+            "customCommands"
+          ]);
+
+          description = ''
+            null => Default
+            "nickServMsg" (1) => NickServ (/MSG NickServ + password)
+            "nickServ" (2) => NickServ (/NICKSERV + password)
+            "challengeAuth" (4) => Challenge Auth (username + password)
+            "sasl" (6) => SASL (username + password)
+            "serverPassword" (7) => Server password (/PASS password)
+            "saslExternal" (10) => SASL EXTERNAL (cert)
+            "customCommands" (9) => Custom (Use "commands" field for Auth, like: 'commands = [ "/msg NickServ IDENTIFY my_password" ];' )
+          '';
+
+          default = null;
+        };
+
+        nickname = mkOption {
+          type = nullOr str;
+          default = null;
+          description = "Primary nickname";
+        };
+
+        nickname2 = mkOption {
+          type = nullOr str;
+          default = null;
+          description = "Secondary nickname";
+        };
+
+        options = mkOption {
+          default = null;
+          type = nullOr channelOptions;
+          description = "Channel options";
+          example = literalExample
+            "{ autoconnect = true; useGlobalUserInformation = true; }";
+        };
+
+        password = mkOption {
+          type = nullOr str;
+          default = null;
+          description = "Password";
+        };
+
+        realName = mkOption {
+          type = nullOr str;
+          default = null;
+          description = "Real name";
+        };
+
+        servers = mkOption {
+          type = (listOf str);
+          description = "IRC Server Address List";
+          example =
+            literalExample ''[ "chat.freenode.net" "irc.freenode.net" ]'';
+          default = [ ];
+        };
+
+        userName = mkOption {
+          type = nullOr str;
+          default = null;
+          description = "User name";
+        };
+      };
+    };
+
+  # helpers
+  transformField = k: v: if (v != null) then "${k}=${v}" else null;
+
+  listChar = c: l:
+    if (l != [ ]) then
+      (concatMapStringsSep "\n" (transformField c) l)
+    else
+      null;
+
+  computeFieldsValue = (channel:
+    (toString (if channel.options == null then
+      0
+    else
+      (with channel.options;
+        (if autoconnect then 8 else 0)
+        + (if !connectToSelectedServerOnly then 1 else 0)
+        + (if !bypassProxy then 16 else 0) + (if forceSSL then 4 else 0)
+        + (if acceptInvalidSSLCertificates then 32 else 0)
+        + (if useGlobalUserInformation then 2 else 0)))));
+
+  loginMethod = let
+    loginMethodMap = {
+      nickServMsg = 1;
+      nickServ = 2;
+      challengeAuth = 4;
+      sasl = 6;
+      serverPassword = 7;
+      saslExternal = 10;
+      customCommands = 9;
+    };
+  in (channel:
+    transformField "L" (optionalString (channel.loginMethod != null)
+      (toString loginMethodMap.${channel.loginMethod})));
+
+  # Note: Missing option `D=`.
+  transformChannel = (channelName:
+    let channel = cfg.channels.${channelName};
+    in concatStringsSep "\n" (filter (v: v != null) [
+      "" # leave a space between one server and another
+      (transformField "N" channelName)
+      (loginMethod channel)
+      (transformField "E" channel.charset)
+      (transformField "F" (computeFieldsValue channel))
+      (transformField "I" channel.nickname)
+      (transformField "i" channel.nickname2)
+      (transformField "R" channel.realName)
+      (transformField "U" channel.userName)
+      (transformField "P" channel.password)
+      (listChar "S" channel.servers)
+      (listChar "J" channel.autojoin)
+      (listChar "C" channel.commands)
+    ]));
 in {
   meta.maintainers = with maintainers; [ superherointj thiagokokada ];
 
   options.programs.hexchat = with types; {
-    channels = let
-      modChannelOption = submodule {
-        options = {
-          autojoin = mkOption {
-            type = listOf str;
-            default = [ ];
-            description = "Channels list to autojoin on connecting to server.";
-            example = literalExample ''[ "##linux" "#nix" ]'';
-          };
-          charset = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Charset";
-            example = "UTF-8 (Unicode)";
-          };
-          commands = mkOption {
-            type = listOf str;
-            description = "Commands to be executed on connecting to server.";
-            example = literalExample ''[ "ECHO Greetings fellow Nixer! ]'';
-            default = [ ];
-          };
-          loginMethod = mkOption {
-            type = nullOr (enum [
-              "nickServMsg"
-              "nickServ"
-              "challengeAuth"
-              "sasl"
-              "serverPassword"
-              "saslExternal"
-              "customCommands"
-            ]);
-            description = ''
-              null => Default
-              "nickServMsg" (1) => NickServ (/MSG NickServ + password)
-              "nickServ" (2) => NickServ (/NICKSERV + password)
-              "challengeAuth" (4) => Challenge Auth (username + password)
-              "sasl" (6) => SASL (username + password)
-              "serverPassword" (7) => Server password (/PASS password)
-              "saslExternal" (10) => SASL EXTERNAL (cert)
-              "customCommands" (9) => Custom (Use "commands" field for Auth, like: 'commands = [ "/msg NickServ IDENTIFY my_password" ];' )
-            '';
-            default = null;
-          };
-          nickname = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Primary nickname";
-          };
-          nickname2 = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Secondary nickname";
-          };
-          options = let
-            channelOptions = submodule {
-              options = {
-                autoconnect = mkOption {
-                  type = nullOr bool;
-                  description = "Autoconnect to network";
-                  default = false;
-                };
-                connectToSelectedServerOnly = mkOption {
-                  type = nullOr bool;
-                  description = "Connect to selected server only";
-                  default = true;
-                };
-                bypassProxy = mkOption {
-                  type = nullOr bool;
-                  description = "Bypass proxy";
-                  default = true;
-                };
-                forceSSL = mkOption {
-                  type = nullOr bool;
-                  description = "Use SSL for all servers";
-                  default = false;
-                };
-                acceptInvalidSSLCertificates = mkOption {
-                  type = nullOr bool;
-                  description = "Accept invalid SSL certificates";
-                  default = false;
-                };
-                useGlobalUserInformation = mkOption {
-                  type = nullOr bool;
-                  description = "Use global user information";
-                  default = false;
-                };
-              };
-            };
-          in mkOption {
-            default = null;
-            type = nullOr channelOptions;
-            description = "Channel options";
-            example = literalExample
-              "{ autoconnect = true; useGlobalUserInformation = true; }";
-          };
-          password = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Password";
-          };
-          realName = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "Real name";
-          };
-          servers = mkOption {
-            type = (listOf str);
-            description = "IRC Server Address List";
-            example =
-              literalExample ''[ "chat.freenode.net" "irc.freenode.net" ]'';
-            default = [ ];
-          };
-          userName = mkOption {
-            type = nullOr str;
-            default = null;
-            description = "User name";
-          };
-        };
-      };
-    in mkOption {
+    enable = mkEnableOption "HexChat - Graphical IRC client";
+
+    channels = mkOption {
       default = null;
       type = nullOr (attrsOf modChannelOption);
       description = "Configures '~/.config/hexchat/servlist.conf'";
@@ -134,7 +211,7 @@ in {
           freenode = {
             autojoin = [
               "#home-manager"
-              "##linux"
+              "#linux"
               "#nixos"
             ];
             charset = "UTF-8 (Unicode)";
@@ -162,6 +239,7 @@ in {
           };
         }'';
     };
+
     settings = mkOption {
       default = null;
       description = ''
@@ -175,23 +253,33 @@ in {
           irc_username = "bob";
           irc_realname = "Bart Simpson";
           text_font = "Monospace 14";
-        };'';
+        };
+      '';
       type = nullOr (attrsOf str);
     };
-    enable = mkEnableOption "HexChat - Graphical IRC client";
+
     overwriteConfigFiles = mkOption {
       type = nullOr bool;
       description = ''
-        Enables overwritting HexChat configuration files (hexchat.conf, servlist.conf). Any existing HexChat configuration will be lost. Certify to back-up any previous configuration before enabling this.
-        Enabling this setting is recommended, because everytime HexChat application is closed it overwrites Nix/Home-Manager provided configuration files, causing:
+        Enables overwritting HexChat configuration files (hexchat.conf, servlist.conf).
+        Any existing HexChat configuration will be lost.
+        Certify to back-up any previous configuration before enabling this.
+
+        Enabling this setting is recommended, because everytime HexChat application is
+        closed it overwrites Nix/Home-Manager provided configuration files, causing:
         1. Nix/HM provided configuration to be out of sync with actual active HexChat configuration.
-        2. Blocking Nix/HM updates until configuration files are manually removed.'';
+        2. Blocking Nix/HM updates until configuration files are manually removed.
+      '';
       default = false;
     };
+
     theme = mkOption {
       default = null;
-      description =
-        "Theme package for HexChat. Expects a derivation containing decompressed theme files. '.hct' file format requires unzip decompression, as seen in example.";
+      description = ''
+        Theme package for HexChat.
+        Expects a derivation containing decompressed theme files.
+        '.hct' file format requires unzip decompression, as seen in example.
+      '';
       example = ''
         stdenv.mkDerivation rec {
           name = "hexchat-theme-MatriY";
@@ -202,7 +290,8 @@ in {
           };
           unpackPhase = "unzip ''${src}";
           installPhase = "cp -r . $out";
-        };'';
+        };
+      '';
       type = nullOr package;
     };
   };
@@ -213,68 +302,22 @@ in {
       source = cfg.theme;
       recursive = true;
     };
+
     xdg.configFile."hexchat/hexchat.conf" = mkIf (cfg.settings != null) {
       force = cfg.overwriteConfigFiles;
-      text = let hexchatConf = cfg.settings;
-      in concatMapStringsSep "\n" (x: x + " = " + hexchatConf.${x})
-      (attrNames hexchatConf);
+      text = concatMapStringsSep "\n" (x: x + " = " + cfg.settings.${x})
+        (attrNames cfg.settings);
     };
+
     xdg.configFile."hexchat/servlist.conf" = mkIf (cfg.channels != null)
       (if attrNames cfg.channels == [ ] then {
         text = "";
       } else {
         force = cfg.overwriteConfigFiles;
-        text = let
-          transformChannel = (channelName:
-            let
-              channel = cfg.channels.${channelName};
-              transformField = (k: v:
-                optionalString (v != null) ''
-
-                  ${k}=${v}'');
-              listChar = c: l: concatMapStrings (transformField c) l;
-              loginMethodMap = {
-                nickServMsg = 1;
-                nickServ = 2;
-                challengeAuth = 4;
-                sasl = 6;
-                serverPassword = 7;
-                saslExternal = 10;
-                customCommands = 9;
-              };
-            in let
-              name = transformField "N" channelName;
-              nickname = transformField "I" channel.nickname;
-              nickname2 = transformField "i" channel.nickname2;
-              realName = transformField "R" channel.realName;
-              userName = transformField "U" channel.userName;
-              password = transformField "P" channel.password;
-              charset = transformField "E" channel.charset;
-              loginMethod = transformField "L"
-                (optionalString (channel.loginMethod != null)
-                  (toString loginMethodMap.${channel.loginMethod}));
-              servers = listChar "S" channel.servers;
-              autojoin = listChar "J" channel.autojoin;
-              commands = listChar "C" channel.commands;
-              options = let
-                computeFieldsValue = (with channel.options;
-                  (if channel.options == null then
-                    0
-                  else
-                    ((if autoconnect then 8 else 0)
-                      + (if !connectToSelectedServerOnly then 1 else 0)
-                      + (if !bypassProxy then 16 else 0)
-                      + (if forceSSL then 4 else 0)
-                      + (if acceptInvalidSSLCertificates then 32 else 0)
-                      + (if useGlobalUserInformation then 2 else 0))));
-              in transformField "F" (toString computeFieldsValue);
-              # Note: Missing option `D=`.
-            in name + loginMethod + charset + options + # `D=` position +
-            nickname + nickname2 + realName + userName + password + servers
-            + autojoin + commands);
-        in concatMapStringsSep "\n" transformChannel (attrNames cfg.channels)
-        # Line break required to avoid cropping last field value.
-        + "\n\n";
+        text =
+          (concatMapStringsSep "\n" transformChannel (attrNames cfg.channels))
+          # Line break required to avoid cropping last field value.
+          + "\n\n";
       });
   };
 }
